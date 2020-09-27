b3298ffacbb55c6b562f20ddd9d63536b8435792
Nirmal Patel <nirmal.patel@theonetechnologies.co.in>
2020-09-27 17:25:59 +0530

    Added changes for jwt token

--- InstitutionService/Startup.cs
+++ InstitutionService/Startup.cs
@@ -3,13 +3,20 @@ using InstitutionService.Helper.Abstraction;
 using InstitutionService.Helper.Models;
 using InstitutionService.Helper.Repository;
 using InstitutionService.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace InstitutionService
 {
@@ -57,6 +64,63 @@ namespace InstitutionService
             var appSettingsSection = Configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingsSection);
             var appSettings = appSettingsSection.Get<AppSettings>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidAudience,
+                    ValidIssuer = appSettings.ValidIssuer,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettings.Secret)),
+                    // verify signature to avoid tampering
+                    ValidateLifetime = true, // validate the expiration
+                    RequireExpirationTime = true,
+                    ClockSkew = TimeSpan.FromMinutes(30) // tolerance for the expiration date
+                };
+                x.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        var logger = context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>().CreateLogger(nameof(JwtBearerEvents));
+                        logger.LogError("Authentication failed.", context.Exception);
+                        context.Response.StatusCode = 401;
+                        if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                        {
+                            context.Response.Headers.Add("Token-Expired", "true");
+                        }
+                        context.Response.OnStarting(async () =>
+                        {
+                            context.Response.ContentType = "application/json";
+                            ErrorMessage errorMessage = new ErrorMessage();
+                            errorMessage.Message = "Authentication failed.";
+                            await context.Response.WriteAsync(JsonConvert.SerializeObject(errorMessage));
+                        });
+                        return Task.CompletedTask;
+                    },
+                    OnMessageReceived = context =>
+                    {
+                        return Task.CompletedTask;
+                    },
+                    OnChallenge = context =>
+                    {
+                        var logger = context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>().CreateLogger(nameof(JwtBearerEvents));
+                        logger.LogError("OnChallenge error", context.Error, context.ErrorDescription);
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
