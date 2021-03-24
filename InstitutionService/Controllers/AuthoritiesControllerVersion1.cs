4512525e9f6e1a1710a6ce2901b197550146d4a8
Wael Moughrbel <wmoughrbel@routesme.com>
2021-03-24 13:02:28 +0300

    Replicate Controllers with applying versioning to APIs

---
+++ InstitutionService/Controllers/AuthoritiesControllerVersion1.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using InstitutionService.Abstraction;
+using InstitutionService.Models;
+using InstitutionService.Models.ResponseModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstitutionService.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class AuthoritiesControllerVersion1 : ControllerBase
+    {
+        private readonly IAuthoritiesRepository _authoritiesRepository;
+        public AuthoritiesControllerVersion1(IAuthoritiesRepository authoritiesRepository)
+        {
+            _authoritiesRepository = authoritiesRepository;
+        }
+
+        [HttpPost]
+        [Route("institutions/authorities")]
+        public IActionResult Post(AuthoritiesModel model)
+        {
+            dynamic response = _authoritiesRepository.InsertAuthorities(model);
+            return StatusCode(response.statusCode, response);
+        }
+
+        [HttpGet]
+        [Route("institutions/authorities/{authorityId?}")]
+        public IActionResult Get(string authorityId, [FromQuery] Pagination pageInfo)
+        {
+            dynamic response = _authoritiesRepository.GetAuthorities(authorityId, pageInfo);
+            return StatusCode(response.statusCode, response);
+        }
+
+        [HttpGet]
+        [Route("institutions/{institutionId}/authorities")]
+        public IActionResult GetAuthoritiesByInstitution(string institutionId, [FromQuery] Pagination pageInfo)
+        {
+            dynamic response = _authoritiesRepository.GetAuthoritiesByInstitutionId(institutionId, pageInfo);
+            return StatusCode(response.statusCode, response);
+        }
+
+        [HttpPut]
+        [Route("institutions/authorities")]
+        public IActionResult Put(AuthoritiesModel model)
+        {
+            dynamic response = _authoritiesRepository.UpdateAuthorities(model);
+            return StatusCode(response.statusCode, response);
+        }
+
+        [HttpDelete]
+        [Route("institutions/authorities/{id}")]
+        public IActionResult Delete(string id)
+        {
+            dynamic response = _authoritiesRepository.DeleteAuthorities(id);
+            return StatusCode(response.statusCode, response);
+        }
+    }
+}
