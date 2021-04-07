fb2a2691140f24256ed0567182e8cc9e07fa29ac
Wael Moughrbel <wmoughrbel@routesme.com>
2021-04-07 14:19:17 +0300

    Refactor all used APIs to support versioning

--- InstitutionService/Helper/Models/AppSettings.cs
+++ InstitutionService/Helper/Models/AppSettings.cs
@@ -2,9 +2,6 @@
 {
     public class AppSettings
     {
-        public string Secret { get; set; }
-        public string ValidAudience { get; set; }
-        public string ValidIssuer { get; set; }
         public string Host { get; set; }
     }
 }
